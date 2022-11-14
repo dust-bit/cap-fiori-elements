0686b490b397c7134adef64b530613dc4f933e39
Dustin Groß <dustin.gross@erp.valantic.com>
2022-11-14 13:12:04 +0100

    init

--- srv/service.js
+++ srv/service.js
@@ -2,4 +2,9 @@ const cds = require("@sap/cds");
 
 module.exports = async function (srv) {
   const external = await cds.connect.to("API_BUSINESS_PARTNER");
+
+  srv.on("READ", ["BusinessPartners"], async (req) => {
+    req.query.where("FirstName <> '' and LastName <> ''");
+    return await external.run(req.query);
+  });
 };
